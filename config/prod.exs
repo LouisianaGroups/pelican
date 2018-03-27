52ac25d469fea129840f4845f1ba0046cfe28f44
Nicholas Scheurich <nick@scheurich.me>
2018-03-27 00:30:49 -0500

    Fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,6 @@ use Mix.Config
 config :pelican, PelicanWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "gsx.freighter.cloud", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "secret_key_base")
+  secret_key_base: Map.fetch!System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
